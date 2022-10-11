642d29019a075913b045eceb0f1414d9134a22de
Pingcube <pingcube@Pingcubes-MacBook-Air.local>
2022-10-12 03:26:46 +0530

    npx create-react-hero

--- bin/cli.js
+++ bin/cli.js
@@ -1,1 +1,30 @@
 #!/usr/bin/env node
+
+const { execSync } = require("child_process");
+
+const runCommand = (command) => {
+  try {
+    execSync(`${command}`, { stdio: "inherit" });
+  } catch (err) {
+    console.error(`Failed to execute ${command}`, err);
+    return false;
+  }
+  return true;
+};
+
+const repoName = process.argv[2];
+const gitCheckoutCommand = `git clone https://github.com/anujmv/create-react-hero.git ${repoName}`;
+const installDepsCommand = `cd ${repoName} && npm install`;
+console.log(`Setting up create-react-hero in ${repoName}`);
+
+const checkedOut = runCommand(gitCheckoutCommand);
+if (!checkedOut) process.exit(-1);
+
+console.log(`Installing dependencies for ${repoName}`);
+
+const insalledDeps = runCommand(installDepsCommand);
+
+if (!insalledDeps) process.exit(-1);
+
+console.log(`Your app is ready. To start App`);
+console.log(`cd ${repoName} && npm start`);
